acf93a76adedafcc645509238945b4d59550ae35
phunguyendinh <phu0981838248@gmail.com>
2026-02-22 00:38:03 +0700

    init app

---
+++ src/main/java/com/lifepattern/ai/LifePatternAiApplication.java
@@ -0,0 +1,12 @@
+package com.lifepattern.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifePatternAiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(LifePatternAiApplication.class, args);
+    }
+}
